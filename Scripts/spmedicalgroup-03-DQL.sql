1f31b208c0de768acf408e77766cdba0aa79fd80
flavio-pires <fdnpires@gmail.com>
2020-03-18 11:19:26 -0300

    Atualização endereço

--- Scripts/spmedicalgroup-03-DQL.sql
+++ Scripts/spmedicalgroup-03-DQL.sql
@@ -12,5 +12,21 @@ SELECT * FROM Medico;
 SELECT * FROM Consulta;
 SELECT * FROM Situacao;
 
-SELECT CONVERT (VARCHAR(10),DataNascimento,103)
+--QUANTIDADE TOTAL DE USUARIOS
+SELECT COUNT(*) AS TotalUsuarios FROM Usuario
+
+--CONVERTER A DATA DE NASCIMENTO DO USUARIO PARA O FORMATO MÊS/DIA/ANO
+SELECT CONVERT (VARCHAR(10),DataNascimento,110) AS [MM-DD-YYYY]
 FROM Paciente;
+
+--MOSTRAR OS NOMES DOS MEDICOS NAS CONSULTAS
+SELECT NomeUsuario AS Médico, Descricao, DataConsulta, TituloSituacao, IdPaciente FROM Usuario
+INNER JOIN Medico ON Usuario.IdUsuario = Medico.IdUsuario
+INNER JOIN Consulta ON Consulta.IdMedico = Medico.IdMedico
+INNER JOIN Situacao ON Situacao.IdSituacao = Consulta.IdSituacao
+
+--MOSTRAR OS NOMES DOS PACIENTES NAS CONSULTAS
+SELECT NomeUsuario AS Paciente, Descricao, DataConsulta, TituloSituacao,IdMedico FROM Usuario
+INNER JOIN Paciente ON Usuario.IdUsuario = Paciente.IdUsuario
+INNER JOIN Consulta ON Consulta.IdMedico = Paciente.IdPaciente
+INNER JOIN Situacao ON Situacao.IdSituacao = Consulta.IdSituacao
